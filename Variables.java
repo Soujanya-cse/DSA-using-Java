72f53221910f64c0c507a4920ad430a028c43567
Soujanya <155300672+Soujanya-cse@users.noreply.github.com>
2026-03-07 22:27:12 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Variables{
+	public static void main(String args[]){
+		/* String Name="Soujanya";
+		int Age=21;
+		float Height=5.4f;
+		String City="Udupi";
+
+		System.out.println("Name:"+Name);
+		System.out.println("Age:"+Age);
+		System.out.println("Height:"+Height);
+		System.out.println("City:"+City); */
+
+		/* int a=10;
+		int b=20;
+		int c=a+b;
+
+		System.out.println("Sum="+c); */
+
+		/* int a=5;
+		int b=10;
+
+		System.out.println("Before Swapping:"+"a="+a+" "+"b="+b);		
+
+		int c=a;
+		a=b;
+		b=c;
+
+		System.out.println("After Swapping:"+"a="+a+" "+"b="+b); */
+
+		/* [Area of the Rectangle = length*breadth;
+		   Temperature from celsius to Farheinheat:
+			F=(C*9/5)+32
+		   Simple Interest:
+			SI=(p*t*r)/100] */
+		
+
+		
+
+
+	}
+}
